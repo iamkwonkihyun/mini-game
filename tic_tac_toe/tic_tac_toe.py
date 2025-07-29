124886dcf54dcde9c247cdaf3a2c61ff71ce31eb
0dihyun <00dihyun@gmail.com>
2025-07-29 16:00:04 +0900

    update tic tac toe

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -4,7 +4,8 @@ import time
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def show_board(j):
+def show_board():
+    j = 0
     print("\ | 0 | 1 | 2\n--------------")
     for i in range(5):
         if i % 2 == 0:
@@ -13,14 +14,24 @@ def show_board(j):
         else:
             print("--------------")
 
+winning_lines = [
+    [(0, 0), (0, 1), (0, 2)],  # 가로 1
+    [(1, 0), (1, 1), (1, 2)],  # 가로 2
+    [(2, 0), (2, 1), (2, 2)],  # 가로 3
+    [(0, 0), (1, 0), (2, 0)],  # 세로 1
+    [(0, 1), (1, 1), (2, 1)],  # 세로 2
+    [(0, 2), (1, 2), (2, 2)],  # 세로 3
+    [(0, 0), (1, 1), (2, 2)],  # 대각선 \
+    [(0, 2), (1, 1), (2, 0)],  # 대각선 /
+]
+
 board = [[" " for _ in range(3)] for _ in range(3)]
 round_cnt = 0
-j = 0
 
 while True:
     clear_console()
     
-    show_board(j)
+    show_board()
     
     match round_cnt % 2:
         case 0:
@@ -40,7 +51,7 @@ while True:
             round_cnt += 1
         case 1:
             # 입력 받기
-            user2_input_row, user2_input_col = map(int, input("user2: ").split())
+            user2_input_row, user2_input_col = map(int, input("\nuser2: ").split())
             
             # 중복 확인
             if board[user2_input_row][user2_input_col] != " ":
@@ -54,51 +65,12 @@ while True:
             # 라운드 값 증가
             round_cnt += 1
     
-    if board[0][0] == board[0][1] == board[0][2] != " ": # 1 -
-        clear_console()
-        show_board(j)
-        print("1 user1 win" if board[0][0] == "O" else "1 user2 win")
-        time.sleep(10)
-        break
-    elif board[1][0] == board[1][1] == board[1][2] != " ": # 2 -
-        clear_console()
-        show_board(j)
-        print("2 user1 win" if board[0][0] == "O" else "2 user2 win")
-        time.sleep(10)
-        break
-    elif board[2][0] == board[2][1] == board[2][2] != " ": # 3 -
-        clear_console()
-        show_board(j)
-        print("3 user1 win" if board[0][0] == "O" else "3 user2 win")
-        time.sleep(10)
-        break
-    elif board[0][0] == board[1][0] == board[2][0] != " ": # 1 |
-        clear_console()
-        show_board(j)
-        print("4 user1 win" if board[0][0] == "O" else "4 user2 win")
-        time.sleep(10)
-        break
-    elif board[0][1] == board[1][1] == board[2][1] != " ": # 2 |
-        clear_console()
-        show_board(j)
-        print("5 user1 win" if board[0][0] == "O" else "5 user2 win")
-        time.sleep(10)
-        break
-    elif board[0][2] == board[1][2] == board[2][2] != " ": # 3 |
-        clear_console()
-        show_board(j)
-        print("6 user1 win" if board[0][0] == "O" else "6 user2 win")
-        time.sleep(10)
-        break
-    elif board[0][2] == board[1][1] == board[2][0] != " ": # /
-        clear_console()
-        show_board(j)
-        print("7 user1 win" if board[0][0] == "O" else "7 user2 win")
-        time.sleep(10)
-        break
-    elif board[0][0] == board[1][1] == board[2][2] != " ": # \
-        clear_console()
-        show_board(j)
-        print("8 user1 win" if board[0][0] == "O" else "8 user2 win")
-        time.sleep(10)
-        break
+    for idx, line in enumerate(winning_lines, 1):
+        a, b, c = line
+        if board[a[0]][a[1]] == board[b[0]][b[1]] == board[c[0]][c[1]] != " ":
+            clear_console()
+            show_board()
+            winner = "user1" if board[a[0]][a[1]] == "O" else "user2"
+            print(f"\n{idx} {winner} win")
+            time.sleep(10)
+            break
